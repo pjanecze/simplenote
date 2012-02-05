ba2cce4da7e0bf7d6565ee0ab4328028796601d4
Paweł Janeczek <pjanecze@gmail.com>
2012-02-06 00:01:49 +0100

    no message

---
+++ src/com/pj/simpleNote/NoteManagerActivity.java
@@ -0,0 +1,15 @@
+package com.pj.simpleNote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteManagerActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_note_manager);
+	}
+
+}
